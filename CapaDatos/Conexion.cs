c11179a550fae3fe037b373eb7d0416cfecd2d5d
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-08-26 22:25:22 -0600

    Agregando Capas.

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public SqlConnection conexion = new SqlConnection("Data Source=LAPTOP-GK1DJVHC\\SQLEXPRESS;Initial Catalog=Practica;Integrated Security=True;");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+            return conexion;
+        }
+    }
+}
--- EjercicioPracticoCRUD/Form1.Designer.cs
+++ EjercicioPracticoCRUD/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(219, 378);
+            this.button1.Location = new System.Drawing.Point(71, 378);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 43);
             this.button1.TabIndex = 1;
