876b80eef42fd1b2d147c74af5e1b9e7769e30f1
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-08-29 21:08:06 -0600

    Añadiendo comentarios importantes (para no olvidar xd).

--- CapaDatos/CDProductos.cs
+++ CapaDatos/CDProductos.cs
@@ -10,45 +10,70 @@ namespace CapaDatos
 {
     public class CDProductos
     {
+        //Instancia un nuevo objeto de esta clase.
+        // Se utiliza para gestionar la conexión con la base de datos.
         private CDConexion conexion = new CDConexion();
+        // Este objeto se utiliza para ejecutar comandos SQL o procedimientos almacenados en la base de datos.
         private SqlCommand comando = new SqlCommand();
+        // Este objeto se utiliza para leer los datos obtenidos de la base de datos a través de una consulta.
         private SqlDataReader leer;
+        // Este objeto se utiliza para almacenar los datos obtenidos de la base de datos en formato de tabla.
         private DataTable tabla = new DataTable();
 
+        //Retorna un 'DataTable'.
         public DataTable Mostrar()
         {
+            // Asigna la conexión abierta a la propiedad 'Connection' del objeto 'comando'.
             comando.Connection = conexion.AbrirConexion();
+            // Establece el texto del comando, que en este caso es el nombre del procedimiento almacenado 'MostrarProductos'.
             comando.CommandText = "MostrarProductos";
+            // Indica que el 'CommandText' es un procedimiento almacenado, no una consulta SQL directa.
             comando.CommandType = CommandType.StoredProcedure;
+            // Ejecuta el comando y asigna el resultado a 'leer', permitiendo la lectura de los datos devueltos por el procedimiento almacenado.
             leer = comando.ExecuteReader();
+            // Carga los datos leídos en el 'DataTable' 'tabla'.
             tabla.Load(leer);
+            // Cierra la conexión con la base de datos.
             conexion.CerrarConexion();
+            // Retorna el 'DataTable' que contiene los productos.
             return tabla;
         }
 
+
         public void Insertar(string nombre, string descripcion, string marca, double precio, int stock)
         {
+            // Asigna la conexión abierta a la propiedad 'Connection' del objeto 'comando'.
             comando.Connection = conexion.AbrirConexion();
+            // Establece el texto del comando, que en este caso es el nombre del procedimiento almacenado 'InsertarProductos'.
             comando.CommandText = "InsertarProductos";
+            // Indica que el 'CommandText' es un procedimiento almacenado.
             comando.CommandType = CommandType.StoredProcedure;
 
+            // Agrega los parámetros necesarios para el procedimiento almacenado, usando los valores recibidos como argumentos.
             comando.Parameters.AddWithValue("@nombre", nombre);
             comando.Parameters.AddWithValue("@descrip", descripcion);
             comando.Parameters.AddWithValue("@marca", marca);
             comando.Parameters.AddWithValue("@precio", precio);
             comando.Parameters.AddWithValue("@stock", stock);
 
+            // Ejecuta el comando sin esperar ningún resultado (ya que es una inserción).
             comando.ExecuteNonQuery();
+            // Limpia los parámetros del comando para evitar conflictos en futuras ejecuciones.
             comando.Parameters.Clear();
+            // Cierra la conexión con la base de datos.
             conexion.CerrarConexion();
         }
 
         public void Editar(string nombre, string descripcion, string marca, double precio, int stock, int id)
         {
+            // Asigna la conexión abierta a la propiedad 'Connection' del objeto 'comando'.
             comando.Connection = conexion.AbrirConexion();
+            // Establece el texto del comando, que en este caso es el nombre del procedimiento almacenado 'EditarProductos'.
             comando.CommandText = "EditarProductos";
+            // Indica que el 'CommandText' es un procedimiento almacenado.
             comando.CommandType = CommandType.StoredProcedure;
 
+            // Agrega los parámetros necesarios para el procedimiento almacenado, incluyendo el 'id' del producto a modificar.
             comando.Parameters.AddWithValue("@nombre", nombre);
             comando.Parameters.AddWithValue("@descrip", descripcion);
             comando.Parameters.AddWithValue("@marca", marca);
@@ -67,13 +92,13 @@ namespace CapaDatos
             comando.CommandText = "EliminarProducto";
             comando.CommandType = CommandType.StoredProcedure;
 
+            // Agrega el parámetro necesario para el procedimiento almacenado, usando el 'id' del producto a eliminar.
             comando.Parameters.AddWithValue("@idpro", id);
+            // Ejecuta el comando sin esperar ningún resultado (ya que es una eliminación).
             comando.ExecuteNonQuery();
+            // Limpia los parámetros del comando para evitar conflictos en futuras ejecuciones.
             comando.Parameters.Clear();
             //conexion.CerrarConexion();
-
-
-
 
 
         }
@@ -101,15 +126,21 @@ namespace CapaDatos
         {
             comando.Connection = conexion.AbrirConexion();
             comando.CommandText = "BuscarProducto";
+            // Indica que el 'CommandText' es un procedimiento almacenado.
             comando.CommandType = CommandType.StoredProcedure;
+            // Agrega el parámetro necesario para el procedimiento almacenado, usando el 'nombre' recibido como argumento.
             comando.Parameters.AddWithValue("@nombre", nombre);
 
+            // Ejecuta el comando y asigna el resultado a 'leer', permitiendo la lectura de los datos devueltos por el procedimiento almacenado.
             leer = comando.ExecuteReader();
+            // Carga los datos leídos en el 'DataTable' 'tabla'.
             tabla.Load(leer);
+            //Cierra la conexión
             conexion.CerrarConexion();
 
+            // Limpia los parámetros del comando para evitar conflictos en futuras ejecuciones.
             comando.Parameters.Clear();
-
+            //Retorna
             return tabla;
         }
 
--- EjercicioPracticoCRUD/Form1.cs
+++ EjercicioPracticoCRUD/Form1.cs
@@ -14,54 +14,72 @@ namespace EjercicioPracticoCRUD
 
     public partial class Form1 : Form
     {
-
+        // Esta instancia se utilizará para interactuar con los productos.
         CNProductos objetoCN = new CNProductos();
+        // Se utiliza para realizar operaciones de edición y eliminación.
         public string IdProducto = null;
+        // Inicialmente es 'false', lo que indica que se está en modo de inserción.
         public bool Editar = false;
 
         public Form1()
         {
+            //Inicializa los componentes del formulario
             InitializeComponent();
         }
 
         public void Form1_Load(object sender, EventArgs e)
         {
+            // Llama al método 'MostrarProductos' para cargar los productos en el DataGridView.
             MostrarProductos();
         }
 
         public void MostrarProductos()
         {
+            // Establece la fuente de datos del 'dataGridView1' como el resultado del método 'MostrarProd' de la capa de negocio.
             dataGridView1.DataSource = objetoCN.MostrarProd();
+            
+            // Refresca el DataGridView para asegurarse de que se muestren los datos actualizados.
+            dataGridView1.Refresh();
         }
 
         public void btnGuardar_Click(object sender, EventArgs e)
         {
+            // Si no se está en modo de edición (es decir, es una inserción de un nuevo producto):
             if (Editar == false)
             {
                 try
                 {
+                    // Llama al método 'InsertarPRod' de la capa de negocio para insertar un nuevo producto con los datos del formulario.
                     objetoCN.InsertarPRod(txtNombre.Text, txtDescripcion.Text, txtMarca.Text, txtPrecio.Text, txtStock.Text);
                     MessageBox.Show("Se insertó correctamente.");
+                    // Actualiza el DataGridView para mostrar los productos después de la inserción.
                     MostrarProductos();
-                    limpiarForm();
+                    limpiarForm(); // Limpia los campos del formulario.
                 }
                 catch (Exception ex)
                 {
+                    // Muestra un mensaje de error en caso de que la inserción falle.
                     MessageBox.Show("No es posible insertar datos por: " + ex.Message);
                 }
             }
             else if (Editar == true)
             {
+                // Si se está en modo de edición:
                 try
                 {
+                    // Llama al método 'EditarProd' de la capa de negocio para editar un producto existente con los datos del formulario y el ID del producto seleccionado.
                     objetoCN.EditarProd(txtNombre.Text, txtDescripcion.Text, txtMarca.Text, Convert.ToDouble(txtPrecio.Text), Convert.ToInt32(txtStock.Text), Convert.ToInt32(IdProducto));
                     MessageBox.Show("Se editó correctamente.");
+                    // Actualiza el DataGridView para mostrar los productos después de la edición.
                     MostrarProductos();
+                    // Limpia los campos del formulario.
                     limpiarForm();
+                    // Restaura el modo de inserción al desactivar el modo edición.
                     Editar = false;
                 }
                 catch (Exception ex)
                 {
+                    // Muestra un mensaje de error en caso de que la edición falle.
                     MessageBox.Show("No es posible editar datos por: " + ex.Message);
                 }
             }
@@ -69,16 +87,20 @@ namespace EjercicioPracticoCRUD
 
         public void btnEditar_Click(object sender, EventArgs e)
         {
+            // Verifica que haya al menos una fila seleccionada en el DataGridView:
             if (dataGridView1.SelectedRows.Count > 0)
             {
+                // Activa el modo edición.
                 Editar = true;
 
+                // Rellena los campos del formulario con los datos de la fila seleccionada.
                 txtNombre.Text = dataGridView1.CurrentRow.Cells["Nombre"].Value.ToString();
                 txtDescripcion.Text = dataGridView1.CurrentRow.Cells["Descripcion"].Value.ToString();
                 txtMarca.Text = dataGridView1.CurrentRow.Cells["Marca"].Value.ToString();
                 txtPrecio.Text = dataGridView1.CurrentRow.Cells["Precio"].Value.ToString();
                 txtStock.Text = dataGridView1.CurrentRow.Cells["Stock"].Value.ToString();
 
+                // Almacena el ID del producto seleccionado en 'IdProducto' para poder editarlo.
                 IdProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
             }
             else
@@ -89,6 +111,7 @@ namespace EjercicioPracticoCRUD
 
         public void limpiarForm()
         {
+            // Limpia todos los campos del formulario, dejándolos vacíos.
             txtNombre.Clear();
             txtDescripcion.Clear();
             txtMarca.Clear();
@@ -118,20 +141,22 @@ namespace EjercicioPracticoCRUD
 
 
 
-
         }
 
 
         //Solo se puede usando el 'Nombre del Producto'.
         public void txtBuscar_TextChanged(object sender, EventArgs e)
         {
+            //Llamamos al método para filtrar
             filtro();
         }
 
         public void filtro()
         {
+            // Reinicializa el objeto 'objetoCN' de la capa de negocio.
             objetoCN = new CNProductos();
 
+            // Obtiene el texto de búsqueda del cuadro de texto 'txtBuscar'.
             string nombreProducto = txtBuscar.Text;
 
             // Llamada al método Buscar y asignación del resultado a un DataGridView
@@ -141,16 +166,16 @@ namespace EjercicioPracticoCRUD
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
-            
-                //MessageBox.Show("Por favor, selecciona una fila.");
-
-                if (dataGridView1.SelectedRows.Count > 0)
+            // Verifica que haya al menos una fila seleccionada en el DataGridView:
+            if (dataGridView1.SelectedRows.Count > 0)
                 {
+                    // Obtiene el ID del producto seleccionado.
                     IdProducto = dataGridView1.CurrentRow.Cells["Id"].Value.ToString();
+                    // Llama al método 'EliminarPRod' de la capa de negocio para eliminar el producto seleccionado.
                     objetoCN.EliminarPRod(IdProducto);
                     MessageBox.Show("Eliminado correctamente.");
-                    MostrarProductos();
-                }
+                    MostrarProductos(); // Actualiza el DataGridView.
+            }
                 else
                 {
                     MessageBox.Show("Por favor, selecciona una fila.");
